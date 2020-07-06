2758c345b4838dd1b25f4145ac41d170f758fc7c
weegeedoescoding <67148946+weegeedoescoding@users.noreply.github.com>
2020-07-06 14:46:33 +0100

    added the lua file
    not much

---
+++ some lua code.lua
@@ -0,0 +1,10 @@
+age = 13
+
+if age < 16 then
+io.write (''You can go to school'', ''\n'')
+local localvar = 10
+elseif (age <= 16) and (age < 18) then
+io.write (''You can drive'', ''\n'')
+else
+io.write (''You can vote'', ''\n'')
+end
